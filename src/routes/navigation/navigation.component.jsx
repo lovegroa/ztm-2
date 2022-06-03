bba0d1c3c963530bdbf6621488b7c721b7971fcb
lovegroa <66517776+lovegroa@users.noreply.github.com>
2022-06-03 20:06:46 +0100

    pushing

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,27 +1,44 @@
-import { Link, Outlet } from 'react-router-dom'
+import { useContext } from 'react';
+import { Outlet } from 'react-router-dom';
 
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
-import './navigation.styles.scss'
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
+
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+
+import { NavigationContainer, NavLinks, NavLink, LogoContainer } from './navigation.styles';
 
 const Navigation = () => {
+	const { currentUser } = useContext(UserContext);
+	const { isCartOpen } = useContext(CartContext);
+
 	return (
 		<>
-			<div className='navigation'>
-				<Link className='logo-container' to='/'>
+			<NavigationContainer>
+				<LogoContainer to='/'>
 					<CrwnLogo className='logo' />
-				</Link>
-				<div className='nav-links-container'>
-					<Link className='nav-link' to='/shop'>
-						SHOP
-					</Link>
-					<Link className='nav-link' to='/auth'>
-						SIGN IN
-					</Link>
-				</div>
-			</div>
+				</LogoContainer>
+				<NavLinks>
+					<NavLink to='/shop'>SHOP</NavLink>
+
+					{currentUser ? (
+						<NavLink as='span' onClick={signOutUser}>
+							SIGN OUT
+						</NavLink>
+					) : (
+						<NavLink to='/auth'>SIGN IN</NavLink>
+					)}
+					<CartIcon />
+				</NavLinks>
+				{isCartOpen && <CartDropdown />}
+			</NavigationContainer>
 			<Outlet />
 		</>
-	)
-}
+	);
+};
 
-export default Navigation
+export default Navigation;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,14 +1,16 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app';
 import {
 	getAuth,
 	signInWithRedirect,
 	signInWithPopup,
 	GoogleAuthProvider,
 	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword
-} from 'firebase/auth'
+	signInWithEmailAndPassword,
+	signOut,
+	onAuthStateChanged,
+} from 'firebase/auth';
 
-import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore'
+import { getFirestore, doc, setDoc, getDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyAd2Jqd6fLU2PQeoHGmTEfcE7NvK5VkxVw',
@@ -16,53 +18,85 @@ const firebaseConfig = {
 	projectId: 'crwn-clothing-db-80674',
 	storageBucket: 'crwn-clothing-db-80674.appspot.com',
 	messagingSenderId: '1052910298564',
-	appId: '1:1052910298564:web:e34cb88a97cd7c97023091'
-}
+	appId: '1:1052910298564:web:e34cb88a97cd7c97023091',
+};
 
-const firebaseApp = initializeApp(firebaseConfig)
+// const firebaseApp = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider()
+const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-	prompt: 'select_account'
-})
+	prompt: 'select_account',
+});
 
-export const auth = getAuth()
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
-export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider)
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
 
-export const db = getFirestore()
+export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+	const collectionRef = collection(db, collectionKey);
+	const batch = writeBatch(db);
+
+	objectsToAdd.forEach((object) => {
+		const docRef = doc(collectionRef, object.title.toLowerCase());
+		batch.set(docRef, object);
+	});
+
+	await batch.commit();
+	console.log('done');
+};
+
+export const getCategoriesAndDocuments = async () => {
+	const collectionRef = collection(db, 'categories');
+	const q = query(collectionRef);
+
+	const querySnapshot = await getDocs(q);
+	const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+		const { title, items } = docSnapshot.data();
+		acc[title.toLowerCase()] = items;
+		return acc;
+	}, {});
+
+	return categoryMap;
+};
 
 export const createUserDocumentFromAuth = async (userAuth, addtionalInformation = {}) => {
-	if (!userAuth) return
-	const userDocRef = doc(db, 'users', userAuth.uid)
+	if (!userAuth) return;
+	const userDocRef = doc(db, 'users', userAuth.uid);
 
-	const userSnapshot = await getDoc(userDocRef)
+	const userSnapshot = await getDoc(userDocRef);
 
 	if (!userSnapshot.exists()) {
-		const { displayName, email } = userAuth
-		const createdAt = new Date()
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
 
 		try {
 			await setDoc(userDocRef, {
 				displayName,
 				email,
 				createdAt,
-				...addtionalInformation
-			})
+				...addtionalInformation,
+			});
 		} catch (error) {
-			console.log('error creating the user', error.message)
+			console.log('error creating the user', error.message);
 		}
 	}
 
-	return userDocRef
-}
+	return userDocRef;
+};
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
-	if (!email || !password) return
-	return createUserWithEmailAndPassword(auth, email, password)
-}
+	if (!email || !password) return;
+	return createUserWithEmailAndPassword(auth, email, password);
+};
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
-	if (!email || !password) return
-	return createUserWithEmailAndPassword(auth, email, password)
-}
+	if (!email || !password) return;
+	return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
