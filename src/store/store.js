bf4d296a3c8c7e9ad0a0f3698a0f17b04e04c185
lovegroa <66517776+lovegroa@users.noreply.github.com>
2022-06-06 20:08:58 +0100

    with thunk

--- src/store/store.js
+++ src/store/store.js
@@ -2,18 +2,20 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import logger from 'redux-logger';
 import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
 
 const persistConfig = {
 	key: 'root',
 	storage,
-	blacklist: ['user'],
+	// blacklist: ['user'],
+	whitelist: ['cart'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
 
 // Checks to see if redux dev tools are installed, and if so uses the context associated with the dev tools
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
